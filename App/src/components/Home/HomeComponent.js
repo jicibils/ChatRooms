af5ef921a74808bcb2c39c5e5a3b0ea6cf17e681
juan ignacio cibils <jicibils@juans-MacBook-Pro-2.local>
2019-12-16 02:17:21 -0300

    feat(Home) add Home component

---
+++ App/src/components/Home/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+class HomeComponent extends React.Component {
+  render() {
+    return (
+      <Grid item xs={12} container justify="center" alignItems="center"></Grid>
+    );
+  }
+}
+
+export default HomeComponent;
